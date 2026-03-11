2151c2b14b035316fb29be4586a1aebba500e3c2
Thiago Fernandes <thfernan1085@gmail.com>
2026-03-11 19:55:57 -0300

    Corrige CORS e cria usuario padrao no banco

--- src/mini_bank_clj/core.clj
+++ src/mini_bank_clj/core.clj
@@ -1,23 +1,41 @@
 (ns mini-bank-clj.core
-  (:require [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
-            [mini-bank-clj.routes :refer [app-routes]]
-            [mini-bank-clj.db.schema :as schema])
+  (:require
+   [clojure.string :as str]
+   [ring.adapter.jetty :refer [run-jetty]]
+   [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+   [mini-bank-clj.routes :refer [app-routes]]
+   [mini-bank-clj.db.schema :as schema])
   (:gen-class))
+
+(defn allowed-origin? [origin]
+  (or (= origin "http://localhost:5173")
+      (and origin (str/ends-with? origin ".vercel.app"))))
+
+(defn cors-headers [origin]
+  {"Access-Control-Allow-Origin" origin
+   "Access-Control-Allow-Headers" "Content-Type, Authorization"
+   "Access-Control-Allow-Methods" "GET, POST, PUT, DELETE, OPTIONS"
+   "Vary" "Origin"})
+
+(defn wrap-cors [handler]
+  (fn [request]
+    (let [origin (get-in request [:headers "origin"])]
+      (if (allowed-origin? origin)
+        (if (= :options (:request-method request))
+          {:status 200
+           :headers (cors-headers origin)
+           :body ""}
+          (let [response (handler request)]
+            (update response :headers merge (cors-headers origin))))
+        (handler request)))))
 
 (def app
   (-> app-routes
+      wrap-cors
       (wrap-json-body {:keywords? true})
       wrap-json-response))
 
-(defn get-port []
-  (Integer/parseInt (or (System/getenv "PORT") "3000")))
-
 (defn -main []
   (schema/init-db)
-  (let [port (get-port)]
-    (println (str "Servidor rodando em http://0.0.0.0:" port))
-    (run-jetty app
-               {:host "0.0.0.0"
-                :port port
-                :join? false})))
+  (println "🚀 Mini Bank API rodando na porta 3000")
+  (run-jetty app {:port 3000 :join? false}))
