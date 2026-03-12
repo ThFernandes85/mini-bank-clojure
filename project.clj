f0f3793f00545e171c2bcb814628fa94f56630f7
Thiago Fernandes <thfernan1085@gmail.com>
2026-03-11 23:05:24 -0300

    fix cors dependency

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com"
 
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring/ring-core "1.12.1"]
-                 [ring/ring-jetty-adapter "1.12.1"]
+                 [ring/ring-core "1.9.6"]
+                 [ring/ring-jetty-adapter "1.9.6"]
                  [ring/ring-json "0.5.1"]
-                 [compojure "1.7.1"]
-                 [org.clojure/java.jdbc "0.7.12"]
-                 [org.xerial/sqlite-jdbc "3.46.0.0"]
-                 [org.postgresql/postgresql "42.7.3"]
+                 [ring-cors "0.1.13"]
+                 [compojure "1.7.0"]
+                 [org.xerial/sqlite-jdbc "3.42.0.0"]
+                 [buddy/buddy-auth "3.0.323"]
                  [buddy/buddy-hashers "2.0.167"]
                  [buddy/buddy-sign "3.5.351"]]
 
